532c522f3e1f9d69ad495169bc084f1837e0cafc
Ymizumura <awphack@gmail.com>
2022-02-10 18:15:52 +0900

    webpack babel typescript でのReact開発環境の作成

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const outputPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: 'development',
+
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    entry: './src/index.tsx',
+    output: {
+        // バンドルしてmain.jsとして出力
+        filename: 'main.js',
+        path: outputPath
+    },
+    devServer: {},
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                // 拡張子 .ts の場合
+                test: /\.tsx$/,
+                // TypeScript をコンパイルする
+                use: 'ts-loader',
+            },
+        ],
+    },
+    // import 文で .ts ファイルを解決するため
+    // これを定義しないと import 文で拡張子を書く必要が生まれる。
+    // フロントエンドの開発では拡張子を省略することが多いので、
+    // 記載したほうがトラブルに巻き込まれにくい。
+    resolve: {
+        // 拡張子を配列で指定
+        extensions: [
+            '.ts', '.js','.tsx'
+        ],
+    },
+};
