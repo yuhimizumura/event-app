fc9d2ad92e43f4a6121737e1e0400e8abcfb3d13
Y.mizumura <y-mizumura@aidma.net>
2022-06-01 18:18:43 +0900

    トークン周り調整

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,14 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {AmplifyAuthenticator,AmplifySignUp,AmplifySignOut} from "@aws-amplify/ui-react";
 import {AuthState, onAuthUIStateChange} from '@aws-amplify/ui-components';
 import {useDispatch, useSelector} from "react-redux";
 import authStatusSet from "../../redux/actions/auth";
 import {isEmpty} from "../../util/util";
-
+import {Redirect} from "react-router-dom"
 const Login = () => {
     const [authState, setAuthState] = React.useState();
     const [user, setUser] = React.useState();
+    const [token,setToken] = useState("")
     const dispatch = useDispatch()
     const state = useSelector(state => state)
 
@@ -18,12 +19,11 @@ const Login = () => {
             setAuthState(nextAuthState);
             setUser(authData)
             if (isEmpty(nextAuthState) || isEmpty(authData)) return
-            console.log(nextAuthState)
-            console.log(authData.signInUserSession.accessToken.getJwtToken())
             data = {
                 "nextAuthState":nextAuthState,
             }
             dispatch(authStatusSet(data))
+            localStorage.setItem("jwToken",authData.signInUserSession.accessToken.getJwtToken())
             localStorage.setItem("status",nextAuthState)
         });
     }, []);
@@ -32,7 +32,8 @@ const Login = () => {
     return authState === AuthState.SignedIn && user ? (
         <div className="App">
             <div>Hello, {user.username}</div>
-            <AmplifySignOut />
+            {/*<AmplifySignOut />*/}
+            <Redirect to={{pathname:"/mypage",state:{token:localStorage.getItem("jwToken")}}} />
         </div>
         ) : (
         <AmplifyAuthenticator>
--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import Header from "../../component/header/Header";
 import QrHome from "../../component/qrreader/qrCodeApp";
-import {AmplifySignOut} from "@aws-amplify/ui-react";
+import {AmplifyAuthenticator,AmplifySignOut} from "@aws-amplify/ui-react";
 import {useSelector} from "react-redux";
+import {Redirect,useLocation} from "react-router-dom"
 
 const MyPage = () => {
 
+    let location = useLocation();
     const state = useSelector(state => state)
-    console.log(state)
-    console.log(localStorage.getItem("status"))
+    console.log(location)
+    let   from   = location.state || { from: { pathname: "/mypage" } };
+
+    console.log(from)
+
+    const removeToken = () => {
+        // window.location.href = "/"
+    }
 
     return (
         <div className="App">
             {/*<QrHome />*/}
             <h2>ここはマイページです</h2>
-            <AmplifySignOut />
+
+           <AmplifyAuthenticator>
+               <AmplifySignOut onClick={() => removeToken()} />
+           </AmplifyAuthenticator>
         </div>
     );
 }
